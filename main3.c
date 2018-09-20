5c6ee76849798d902d18e8c6d91cba1417be0901
danik529 <lsy12011@naver.com>
2018-09-20 16:47:35 +0900

    03 change

---
+++ main3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	char c;
+	int t;
+	
+	printf("infput a character :");
+	scanf("%c", &c);
+	
+	printf("The next character of %c is %c \n", c, c+1);
+	
+	return 0;
+}
